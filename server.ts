dc99e8063bdd6db5efb5ac69266062d3c673dd39
xgwht2913163-lab <xgwht2913163@gmail.com>
2026-06-12 19:02:49 +0800

    Add files via upload

--- server.ts
+++ server.ts
@@ -8,7 +8,7 @@ dotenv.config();
 
 async function startServer() {
   const app = express();
- const PORT = Number(process.env.PORT) || 3000;
+  const PORT = Number(process.env.PORT) || 3000;
 
   app.use(express.json());
 
